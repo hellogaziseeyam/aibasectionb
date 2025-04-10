96ad3eacbf2f697d364051aa8f94ad0b2e1e4f31
Gazi Seeyam <hellogaziseeyam@gmail.com>
2025-04-10 20:41:16 +0600

    changed database

--- portal/settings.py
+++ portal/settings.py
@@ -59,14 +59,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'portal.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
-} if os.environ.get('DATABASE_URL') else {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Static files (CSS, JS, Images)
 STATIC_URL = '/static/'
